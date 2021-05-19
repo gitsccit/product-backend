2fcc5405cfb9782f141a5504396ed6930055c175
James Chen <xchen@sourcecode.com>
2021-05-19 14:58:46 -0400

    product compare

--- templates/element/compare.php
+++ templates/element/compare.php
@@ -3,7 +3,7 @@
  * @var \App\View\AppView $this
  * @var array $products
  */
-$this->Html->script('product_compare', ['block' => true]);
+$this->Html->script('ProductBackend.product_compare', ['block' => true]);
 ?>
 
 <div class="modal fade" id="compare-modal" tabindex="-1" role="dialog" aria-hidden="true">
---
+++ webroot/js/product_compare.js
@@ -0,0 +1,40 @@
+function find_base_url() {
+    let url = window.location.href;
+    let index = url.indexOf('/hardware');
+
+    return url.substr(0, index) + '/hardware/compare';
+}
+
+function product_compare(currentProduct, check = true) {
+    let url = find_base_url();
+
+    const currentCheckbox = document.getElementsByName(currentProduct)[0];
+    currentCheckbox.checked = check;
+    if (check) {
+        url += `/${currentCheckbox.name}`;
+    }
+
+    const checkboxes = document.getElementsByClassName('product-compare');
+    for (const checkbox of checkboxes) {
+        if (checkbox.checked && checkbox.name != currentProduct) {
+            url += `/${checkbox.name}`;
+        }
+    }
+
+    let request = new XMLHttpRequest();
+    const compareModal = document.querySelector('#compare-modal-content');
+    request.onloadstart = function () {
+        compareModal.innerHTML = `
+            <div class="spinner-border text-primary align-self-center" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>`;
+    };
+    request.onreadystatechange = function () {
+        compareModal.innerHTML = request.response;
+    };
+
+    request.open('GET', url);
+    request.send();
+
+    return false;
+}
