c1750bc8a08bfd5ec38afc52292271bc88788768
James Chen <xchen@sourcecode.com>
2021-05-19 15:11:52 -0400

    products

--- templates/element/cards/product_1.php
+++ templates/element/cards/product_1.php
@@ -6,24 +6,27 @@
 
 $url = $this->Url->build("/product/$card[url]");
 ?>
-<div class="d-flex py-2">
-    <div class="d-flex flex-wrap bg-3 flex-fill shadow">
-        <div class="col-lg-3 d-flex flex-column align-items-center px-0 bg-white">
-            <a class="d-flex align-items-center justify-content-center p-5 w-100" href="<?= $url ?>"
-               style="height: 150px">
-                <img class="mw-100 mh-100"
-                     src="<?= \ProductBackend\Core\Utility::getFileUrl($card['image_id'], 100, 70) ?>"
-                     alt="<?= $card['name'] ?>">
-            </a>
-            <label class="mt-3 my-2 d-flex align-items-center" for="compare">
-                <input class="product-compare" type="checkbox" name="<?= $card['id'] ?>">
-                <a class="ms-1 text-dark" href="javascript:void(0)" data-bs-toggle="modal"
-                   data-bs-target="#compare-modal"
-                   onclick="product_compare(<?= $card['id'] ?>)">Compare</a>
-            </label>
+<div class="bg-3 my-3 shadow">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="d-flex flex-column align-items-center bg-white h-100">
+                <a class="d-flex align-items-center justify-content-center p-5 w-100" href="<?= $url ?>"
+                   style="height: 150px">
+                    <img class="mw-100 mh-100"
+                         src="<?= \ProductBackend\Core\Utility::getFileUrl($card['image_id'], 100, 70) ?>"
+                         alt="<?= $card['name'] ?>">
+                </a>
+                <label class="mt-3 my-2 d-flex align-items-center" for="compare">
+                    <input class="product-compare" type="checkbox" name="<?= $card['id'] ?>">
+                    <a class="ms-1 text-dark" href="javascript:void(0)" data-bs-toggle="modal"
+                       data-bs-target="#compare-modal" onclick="product_compare(<?= $card['id'] ?>)">
+                        Compare
+                    </a>
+                </label>
+            </div>
         </div>
         <div class="col-lg-9">
-            <div class="row py-3 h-100">
+            <div class="row p-3 ps-md-0 h-100">
                 <div class="col-lg-8">
                     <div class="d-flex flex-column justify-content-between h-100">
                         <a class="h5 text-black text-on-hover-primary text-decoration-none fw-bold"
@@ -45,7 +48,7 @@ $url = $this->Url->build("/product/$card[url]");
                         </div>
                         <input class="form-control mb-3" style="width: 4rem" type="number" name="quantity"
                                min="1" value="1">
-                        <a class="btn btn-primary text-nowrap" href="#">Add To Order</a>
+                        <a class="btn btn-primary text-nowrap" href="javascript:void(0)">Add To Order</a>
                     </div>
                 </div>
             </div>
