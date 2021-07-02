fb3380b97d9a5cbd8f64f253dbd21b2bf04ad2e1
James Chen <xchen@sourcecode.com>
2021-07-02 14:29:23 -0400

    add to order

--- templates/Systems/view_1.php
+++ templates/Systems/view_1.php
@@ -36,8 +36,8 @@ $storeID = $this->request->getQuery('store_id', $this->request->getSession()->re
 <div id="configurator" data-tabs='<?= json_encode($tabs, JSON_HEX_APOS) ?>'
      data-system='<?= json_encode($system, JSON_HEX_APOS) ?>'
      data-base-url='<?= trim($this->Url->build('/', ['fullBase' => true]), '/') ?>'
-     data-environment='<?= $environmentID ?>'
-     data-store='<?= $storeID ?>'
+     data-environment-id='<?= $environmentID ?>'
+     data-store-id='<?= $storeID ?>'
      data-token='<?= Configure::read('Security.scctoken') ?>'
     <?= Configure::read('ProductBackend.showCost') ? "data-price-levels='$priceLevels'" : '' ?>
     <?= Configure::read('ProductBackend.showCost') ? "data-current-price-level='$currentPriceLevel'" : '' ?>
