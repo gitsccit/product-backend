feb2e773e64ce1a0a673e46156756da1f0a749b6
James Chen <xchen@sourcecode.com>
2021-11-02 17:30:17 -0400

    added custom scripts for creating db mirrors

--- scripts/create_mirrors.sh
+++ scripts/create_mirrors.sh
@@ -3,7 +3,7 @@
 DB_NAME=product_backend$DB_NAME_SUFFIX
 ACTIVE_DB=${DB_NAME}_mirror_1
 
-mysqldump -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" > product_backend.sql
+mysqldump -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" --ignore-table="$DB_NAME.active_backend_database" > product_backend.sql
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "DROP DATABASE IF EXISTS ${DB_NAME}_mirror_1; DROP DATABASE IF EXISTS ${DB_NAME}_mirror_2; CREATE DATABASE IF NOT EXISTS ${DB_NAME}_mirror_1; CREATE DATABASE IF NOT EXISTS ${DB_NAME}_mirror_2; TRUNCATE TABLE $DB_NAME.active_backend_database; INSERT INTO $DB_NAME.active_backend_database (name) VALUE ('$ACTIVE_DB');"
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "${DB_NAME}_mirror_1" < product_backend.sql
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "${DB_NAME}_mirror_2" < product_backend.sql
