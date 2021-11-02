f4ec587eeea56b1be49e2382c6e228f923949d95
James Chen <xchen@sourcecode.com>
2021-11-02 17:13:30 -0400

    added custom scripts for creating db mirrors

--- scripts/create_mirrors.sh
+++ scripts/create_mirrors.sh
@@ -7,3 +7,4 @@ mysqldump -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" > product_
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "drop database ${DB_NAME}_mirror_1; drop database ${DB_NAME}_mirror_2; create database ${DB_NAME}_mirror_1; create database ${DB_NAME}_mirror_2; truncate table $DB_NAME.active_backend_database; insert into $DB_NAME.active_backend_database (name) value ('$ACTIVE_DB');"
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "${DB_NAME}_mirror_1" < product_backend.sql
 mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "${DB_NAME}_mirror_2" < product_backend.sql
+rm product_backend.sql
