983468b6200d2b2e49353bd815bfc4f01fef701d
James Chen <jchen@sourcecode.com>
2022-06-01 11:44:57 -0400

    no price or stock

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 namespace ProductBackend\Controller\Api;
 
 use App\Controller\Api\AppController as BaseController;
+use Cake\Core\Configure;
 
 class AppController extends BaseController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        Configure::write('ProductBackend', []);
+    }
 }
