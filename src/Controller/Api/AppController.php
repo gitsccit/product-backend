fae879e79de6dfeaa3abc5366fa6b721fe489f92
James Chen <jchen@sourcecode.com>
2022-06-14 14:18:16 -0400

    perspective

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -5,7 +5,6 @@ namespace ProductBackend\Controller\Api;
 
 use App\Controller\Api\AppController as BaseController;
 use Cake\Core\Configure;
-use Cake\ORM\TableRegistry;
 
 class AppController extends BaseController
 {
@@ -15,8 +14,8 @@ class AppController extends BaseController
 
         Configure::write('ProductBackend', []);
 
-        if ($store = $this->request->getQuery('store')) {
-            $this->request->getSession()->write('store.perspective', TableRegistry::getTableLocator()->get('Stores')->get($store)->perspective);
+        if ($perspective = $this->request->getQuery('perspective')) {
+            $this->request->getSession()->write('store.perspective', $perspective);
         }
     }
 }
