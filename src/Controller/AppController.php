52e0463c9b7914f5e10776839d97230029b0aebe
James Chen <jchen@sourcecode.com>
2022-11-02 14:31:46 -0400

    db connection

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 namespace ProductBackend\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 
 class AppController extends BaseController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $activeDB = TableRegistry::getTableLocator()->get('ProductBackend.ActiveBackendDatabase')->find()->first()->name;
+        ConnectionManager::get('product_backend')->execute("USE $activeDB;");
+    }
 }
